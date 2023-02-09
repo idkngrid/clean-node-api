ccc1a5a0d62a43e0c7cc10f050ec09d102b9aaf9
idkngrid <ingrid.camargopr@gmail.com>
2023-02-09 19:41:10 -0300

    feat: add collectCoverage as true in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  // roots: ['<rootDir/>src'],
+  roots: ['src/'],
+
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir/>src/**/*.ts'],
+  collectCoverageFrom: ['src/**/*.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
