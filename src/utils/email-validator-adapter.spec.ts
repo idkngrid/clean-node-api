2e56a7f4ff01d7bd6fac9c9f67ebc12a561335a9
idkngrid <ingrid.camargopr@gmail.com>
2023-04-12 15:51:21 -0300

    refactor: move signUpController creation to a factory helper method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,23 +1,27 @@
 import { EmailValidatorAdapter } from './email-validator-adapter'
 import validator from 'validator'
 
+const makeSignUpController = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator Adapter', () => {
   it('should return false if validator returns false', () => {
-    const signUpController = new EmailValidatorAdapter()
+    const signUpController = makeSignUpController()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = signUpController.isValid('invalid_email@gmail.com')
     expect(isValid).toBe(false)
   })
 
   it('should return true if validator returns true', () => {
-    const signUpController = new EmailValidatorAdapter()
+    const signUpController = makeSignUpController()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
     const isValid = signUpController.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
 
   it('should call validator with correct email', () => {
-    const signUpController = new EmailValidatorAdapter()
+    const signUpController = makeSignUpController()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     signUpController.isValid('any_email@mail.com')
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
