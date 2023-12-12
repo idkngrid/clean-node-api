c3a0256fa3c41a5468cde6a12326bc97dc7584f6
idkngrid <ingrid.camargopr@gmail.com>
2023-12-12 11:00:02 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { type HttpRequest, type HttpResponse, type Controller } from '../../protocols'
 import { type EmailValidator } from '../signup/signup-protocols'
 
@@ -11,26 +11,30 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))) })
-    }
-
-    if (!password) {
-      return new Promise(resolve => { resolve(badRequest(new MissingParamError('password'))) })
-    }
-
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-
-    return {
-      statusCode: 200,
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password'
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))) })
       }
+
+      if (!password) {
+        return new Promise(resolve => { resolve(badRequest(new MissingParamError('password'))) })
+      }
+
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      return {
+        statusCode: 200,
+        body: {
+          email: 'any_email@mail.com',
+          password: 'any_password'
+        }
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
