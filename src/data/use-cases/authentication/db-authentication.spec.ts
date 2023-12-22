4bb96c39e1ac5f0e3bee1ceb6d553702419fee76
idkngrid <ingrid.camargopr@gmail.com>
2023-12-22 14:06:25 -0300

    refactor: move DbAuthentication protocols to a single file

--- src/data/use-cases/authentication/db-authentication.spec.ts
+++ src/data/use-cases/authentication/db-authentication.spec.ts
@@ -1,10 +1,12 @@
-import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { type AccountModel } from '../add-account/db-add-account-protocols'
 import { DbAuthentication } from './db-authentication'
-import { type AuthenticationModel } from '../../../domain/use-cases/authentication'
-import { type HashComparer } from '../../protocols/criptography/hash-comparer'
-import { type TokenGenerator } from '../../protocols/criptography/token-generator'
-import { type UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
+import {
+  type LoadAccountByEmailRepository,
+  type TokenGenerator,
+  type UpdateAccessTokenRepository,
+  type AuthenticationModel,
+  type HashComparer,
+  type AccountModel
+} from './db-authentication-protocols'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'any_id',
--- src/data/use-cases/authentication/db-authentication.ts
+++ src/data/use-cases/authentication/db-authentication.ts
@@ -1,8 +1,11 @@
-import { type AuthenticationModel, type Authentication } from '../../../domain/use-cases/authentication'
-import { type HashComparer } from '../../protocols/criptography/hash-comparer'
-import { type TokenGenerator } from '../../protocols/criptography/token-generator'
-import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { type UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
+import {
+  type AuthenticationModel,
+  type Authentication,
+  type HashComparer,
+  type TokenGenerator,
+  type LoadAccountByEmailRepository,
+  type UpdateAccessTokenRepository
+} from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
